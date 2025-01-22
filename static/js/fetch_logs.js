6a9e052c655a51c2803d1bc0850e3df8be6bcbed
akintolatechnologies <akintolatechnologies@gmail.com>
2025-01-22 10:10:00 +0100

    Fetch API implementation

---
+++ static/js/fetch_logs.js
@@ -0,0 +1,41 @@
+
+
+function fetchLogs() {
+    fetch("{% url 'botengine:get_logs' %}")
+        .then(response => response.json())
+        .then(data => {
+            const recentLogsContainer = document.getElementById('recentLogsContainer');
+            const allLogsContainer = document.getElementById('allLogsContainer');
+
+            // Clear both containers
+            recentLogsContainer.innerHTML = '';
+            allLogsContainer.innerHTML = '';
+
+            // Populate recent logs container
+            data.recent_logs.forEach(log => {
+                const logElement = document.createElement('p');
+                logElement.innerHTML = `<strong>${log.counter}.</strong> ${log.details} on ${log.created}`;
+                recentLogsContainer.appendChild(logElement);
+            });
+
+            // Populate all logs container
+            data.all_logs.forEach(log => {
+                const logElement = document.createElement('p');
+                logElement.innerHTML = `<strong>${log.counter}.</strong> ${log.details} on ${log.created}`;
+                allLogsContainer.appendChild(logElement);
+            });
+
+            // Determine the number of items in comprehensive logs
+            const totalLogsCount = data.all_logs.length;
+
+            // Optionally, display the count on the page
+            const totalLogsCountElement = document.querySelector(".log-count");
+            if (totalLogsCountElement) {
+                totalLogsCountElement.textContent = totalLogsCount;
+            }
+        });
+}
+
+// Refresh logs every 10 seconds
+setInterval(fetchLogs, 5000);
+
